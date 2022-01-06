b1e8af090872b2aa0f4330d7c5f00a8fb35a3d2b
sinthath <sinthathirath@gmail.com>
2022-01-05 20:26:03 -0500

    update to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,39 @@
+// DOM variables
+var mrktContainerEl = document.querySelector("market-container");
 var newsContainerEl = document.querySelector("news-container");
+var tokenId = document.getElementById("token-id");
 
-var url = 'https://newsapi.org/v2/everything?' +
-          'q=BTC&' +
+// url variables
+var urlMrkt = 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true'
+
+var urlNews = 'https://newsapi.org/v2/everything?' +
+          'q=' + tokenId + '&' +
           'from=2021-12-27&' +
           'sortBy=popularity&' +
           'apiKey=8796351310214ee8bc74043e9cc1ab9c';
 
-var req = new Request(url);
 
-// make get request to url 
-fetch(req).then(function(response) {
+var reqA = new Request(urlMrkt);
+
+// make get request to market api
+fetch(reqA).then(function(response) {
+    // request successful
+    if (response.ok) {
+        response.json().then(function(data) {
+            displayMrkt(data);
+            // console.log(response.json());
+        })
+
+       } else {
+        // if not successful, return to homepage
+        document.location.replace("./index.html");
+    }    
+});
+
+var reqB = new Request(urlNews);
+
+// make get request to news api  
+fetch(reqB).then(function(response) {
     // request successful
     if (response.ok) {
         response.json().then(function(data) {
@@ -22,6 +46,28 @@ fetch(req).then(function(response) {
         document.location.replace("./index.html");
     }    
 });
+
+var displayMrkt = function(data) {
+    if (data.length === 0) {
+        mrktContainerEl.textContent = "This is not a cryptocurrency."
+        return;
+    }
+
+// loop over market objects
+for (var i=0; i < data.length; i++) {
+    var mrktEl = document.createElement("a");
+}
+
+// create span to hold object title
+var objEl = document.createElement("span");
+objEl.textContent = data[i].title;
+
+// append to container
+mrktEl.appendChild(objEl);
+
+// append to DOM
+mrktContainerEl.appendChild(mrktEl);
+};
 
 var displayNews = function(news) {
     if (news.length === 0) {
