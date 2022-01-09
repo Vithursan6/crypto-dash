2f17c961adea5d37c66e1da9d42aba5a4f375869
Vithursan6 <93847590+Vithursan6@users.noreply.github.com>
2022-01-09 15:07:03 -0500

    updated script.js with form and api logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,111 +5,69 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
-
-var newsContainerEl = document.querySelector("#news-container");
-// var tokenIdEl = document.querySelector("#tokenId");
+// DOM variables
+var mrktContainerEl = document.querySelector("#news-container1");
+//var mrktEl = document.querySelector("#news-container");
+var tokenId2El = document.querySelector("#tokenId");
+var newsContainerEl = document.querySelector("#news-container2");
+var tokenIdEl = document.querySelector("#tokenId");
 var userFormEl = document.querySelector("#user-form");
+var userFormEl2 = document.querySelector("#user-form");
 var coinSearch = document.querySelector("#coin-search");
 
-// var formSubmitHandler = function(event){
+var formSubmitHandler = function(event){
 
-
-//     //prevent page from refreshing
-//     event.preventDefault();
+    //prevent page from refreshing
+    event.preventDefault();
     
-//     var tokenId = tokenIdEl.value.toUpperCase();
+    var tokenId = tokenIdEl.value.toUpperCase();
     
-//     if(tokenId) {
-//         getTokenInfo(tokenId);
+    if(tokenId) {
+        getTokenInfo(tokenId);
 
-//     }
-//     console.log(tokenId);
+    }
+    console.log(tokenId);
 
-// };
-
-
-// //debugger;
-
-// var getTokenInfo = function(tokenId) {
-
-//     console.log(tokenId);
-
-//     var url = 'https://newsapi.org/v2/everything?q=' + tokenId + '&from=2021-12-27&sortBy=popularity&apiKey=3c69c294543448eb99adc9b1cf4d8e3b';
-
-//     var req = new Request(url);
+};
 
 
+//debugger;
 
-//     // make get request to url 
-//     fetch(req).then(function (response) {
-//         // request successful
-//         if (response.ok) {
-//             response.json().then(function (data) {
-//                 displayNews(data, tokenId);
-//                 console.log(data);
-//             });
-//         } else {
-//             // if not successful, return to homepage
-//             document.location.replace("./index.html");
-//         }
+var getTokenInfo = function(tokenId) {
+
+    console.log(tokenId);
+
+    var url = 'https://newsapi.org/v2/everything?q=' + tokenId + '&from=2021-12-27&sortBy=popularity&apiKey=3c69c294543448eb99adc9b1cf4d8e3b';
+
+    var req = new Request(url);
+
+
+
+    // make get request to url 
+    fetch(req).then(function (response) {
+        // request successful
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayNews(data, tokenId);
+                console.log(data);
+            });
+        } else {
+            // if not successful, return to homepage
+            document.location.replace("./index.html");
+        }
 
   
         
 
       
 
-//     });
+    });
 
    
 
 
-// };
+};
 
-
-
-
-// var displayNews = function(newsApi) {
-//     if (newsApi.length === 0) {
-//         newsContainerEl.textContent = "There are no news items related to this currency."
-//         return;
-
-//     }
-
-//     //coinSearch.textContent = title;
-
-
-
-// debugger;
-// // loop over news items
-// for (var i=0; i < newsApi.length; i++) {
-
-//     var newsName = newsApi[i].articles({title});
-//     var newsEl = document.createElement("a");
-//     newsEl.classList = "list-item flex-row justify-space-between align-center";
-//     //console.log(news[i]);
-
-
-//     // create span to hold news title
-// var titleEl = document.createElement("span");
-// titleEl.textContent = newsName;
-
-// // append to container
-// newsEl.appendChild(titleEl);
-// console.log(newsEl);
-
-// // append to DOM
-// newsContainerEl.appendChild(newsEl);
-// }
-
-
-// };
-
-// DOM variables
-var mrktContainerEl = document.querySelector("#news-container1");
-//var mrktEl = document.querySelector("#news-container");
-var tokenId2El = document.querySelector("#tokenId");
-
-    }
 
 var formSubmitHandler = function(event){
 
@@ -197,8 +155,8 @@ var displayData = function(dataArr) {
         // append to DOM
         //mrktEl.appendChild = objEl.textContent;
         //console.log(mrktEl);
-        //mrktContainerEl = mrktEl.textContent;
-        //console.log(mrktContainerEl);
+        mrktContainerEl = mrktEl.textContent;
+        console.log(mrktContainerEl);
     
     }
 
